695bccc1a11b3566542f6824dcffc81c847598dd
Drew <thedrewwalker@gmail.com>
2019-10-04 17:46:20 -0400

    This is the first effort at combining the spotifyr and scrobblercodes-- still work in progress. We can identify the playlist id but we can't get the tracks yet

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HermioneSpotifyDelirium
 Pulling data from Hermione's account
+
+This repository consists of code relating to pulling the last.fm account for the PICU Spotify delirium project from the account Hermione. 
+
--- TestDrewAccountHermionelastfmscrobble.R
+++ TestDrewAccountHermionelastfmscrobble.R
@@ -1,3 +1,4 @@
+#Install and check the packages from the ScrobbleR package
 install.packages("devtools")
 install.packages("urltools")
 install.packages("jsonlite")
@@ -10,8 +11,24 @@ library(urltools)
 library(jsonlite)
 library(memoise)
 
+#Next we'll install and check the packages from the Spotifyr package that we made
 
-#next you'll call the last.fm api and make a dataframe of all your tracks 
+
+#R Spotify Code
+
+##Install packages, do this once on new computer
+install.packages("devtools")
+devtools::install_github('charlie86/spotifyr')
+install.packages("knitr")
+install.packages("tidyverse")
+install.packages("data.table")
+
+#Begin selection here and extend to bottom of page to run daily. This section below assures the commands are sourced in loaded packages
+library(data.table)
+library(devtools)
+library(spotifyr)
+library(knitr)
+library(tidyverse)
 
 
 my_data <- scrobbler::download_scrobbles(username = "thedrewwalker", api_key = "50d7685d484772f2ff42c45891b31c7b")
@@ -22,6 +39,43 @@ my_data <- scrobbler::download_scrobbles(username = "thedrewwalker", api_key = "
 
 
 
+
+#This sets up system env variables that grant our app authorization to pull GET requests from Spotify API
+Sys.setenv(SPOTIFY_CLIENT_ID = '2c46a5d6764f425ab746a56a1c8791b9')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '9b809cd5be004e8fbbc72ad74b0e19a7')
+
+access_token <- get_spotify_access_token(client_id = Sys.getenv("SPOTIFY_CLIENT_ID"),
+                                         client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET"))
+playlists <- get_my_playlists(limit = 20)
+#That will ask you to enter 1
+1
+#That should open up the browser. 
+#Now, we need to make sure we are selecting the playlist that is the last.fm pull
+playlist_id <- "5D2y9WkW79chB4XpaRG54Y"
+
+
+get_playlist_tracks <- function(playlist_id, fields = NULL, limit = 100, offset = 0, market = NULL, authorization = get_spotify_access_token(), include_meta_info = FALSE) {
+  base_url <- 'https://api.spotify.com/v1/playlists'
+  url <- str_glue('{base_url}/{playlist_id}/tracks')
+  params <- list(
+    fields = paste0('items(', paste0(fields, collapse = ','), ')'),
+    limit = limit,
+    offset = offset,
+    market = market,
+    access_token = authorization
+  )
+  res <- RETRY('GET', url, query = params, encode = 'json')
+  stop_for_status(res)
+  res <- fromJSON(content(res, as = 'text', encoding = 'UTF-8'), flatten = TRUE)
+  
+  if (!include_meta_info) {
+    res <- res$items
+  }
+  return(res)
+}
+
+
+function
 
 
 
