baa7ef3d34fc460343fa5c9f74e69bc8f70a7c61
Drew <Drew@MacBook-Pro.local>
2019-10-11 02:11:39 -0400

    This commit results in a file with local timezones in UTC format, ordered by recency (most recent at top). Need to figure out why there are missing tracks-- likely search error resulting in blank tibble-- is there away we can say N/A instead

--- HermioneLastfmHistoryAndSpotifyTrackAnalysis.R
+++ HermioneLastfmHistoryAndSpotifyTrackAnalysis.R
@@ -5,25 +5,28 @@
 install.packages("tidyverse")
 install.packages("devtools")
 devtools::install_github('charlie86/spotifyr')
+devtools::install_github("condwanaland/scrobbler")
 install.packages("purrr")
 install.packages("dplyr")
 install.packages("Rcpp")
-devtools::install_github("condwanaland/scrobbler")
+install.packages("knitr")
 
 #Added Library installation of package "Rcpp" due to error code in work computer-- may not need if we install Rtools first
-library(devtools)
-library(purrr)
-library(spotifyr)
-library(dplyr)
 library(tidyverse)
-library("scrobbler")
+library(devtools)
+library(spotifyr)
+library(scrobbler)
+library(purrr)
+library(dplyr)
 library(Rcpp)
+library(knitr)
+library(lubridate)
 
-#This is how we create function for getting song features
+#This is how we create function for searching spotify to get song features
 track_audio_features <- function(artist, title, type = "track") {
   search_results <- search_spotify(paste(artist, title), type = type)
   track_audio_feats <- get_track_audio_features(search_results$id[[1]]) %>%
-    dplyr::select(-id, -uri, -track_href, -analysis_url)
+    dplyr::select(-uri, -track_href, -analysis_url)
   return(track_audio_feats)
 }
 # create a version of this function which can handle errors
@@ -36,25 +39,23 @@ my_data <- scrobbler::download_scrobbles(username = "thedrewwalker", api_key = "
 Sys.setenv(SPOTIFY_CLIENT_ID = '2c46a5d6764f425ab746a56a1c8791b9')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '9b809cd5be004e8fbbc72ad74b0e19a7')
 
-access_token <- get_spotify_access_token(client_id = Sys.getenv("SPOTIFY_CLIENT_ID"),
-                                         client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET"))
 #This begins the authorization process, linked to the account most recently signed in
 
 colnames(my_data)
 colnames(my_data)[colnames(my_data)=="song_title"] <- "title"
-##I am acurrently having issues pulling using this, getting error codes consisting of Unknown or uninitialised column: 'id'
+##Now, we'll make a function that applys the search track audio features through the rows of artists and track titles. 
 possible_feats <- possibly(track_audio_features, otherwise = tibble())
 totalaudio_features <- my_data %>%
   mutate(audio_features = map2(artist, title, possible_feats)) %>%
   unnest() %>% 
   as_tibble()
 
-#This works! Although we are missing 9 observations-- 9 songs in this data set, I'm guessing ones that were unable to find
+#This works! Although we are missing 9 observations-- 9 songs in this data set, I'm guessing ones that were unable to find-- can we change the tibble that the function produces for incomplete songs in the possibly? 
 #The following code will create one huge .csv file
 #This index does not work-- because the date format does not decrease in any chronological order-- can fix by converting to table
-ndx <- order(total$played_at,decreasing = TRUE)
+total_audiofeatures
 
-total <- totalaudio_features[ndx,]
+total <- totalaudio_features
 #Then we make a title element that updates with the timestamp of the data pull
 csvFileName <- paste("HermioneGranger",format(Sys.time(),"%d-%b-%Y %H.%M"),".csv")
 #Next we'll make sure this table is a data frame 
