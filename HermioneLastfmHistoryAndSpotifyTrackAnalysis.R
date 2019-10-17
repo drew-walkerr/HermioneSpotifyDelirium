5a92b4606d6024ab80f5b4e875e35e03135629ab
Drew <Drew@MacBook-Pro.local>
2019-10-17 01:20:20 -0400

    added packrat to project to help with package reproducibility

--- HermioneLastfmHistoryAndSpotifyTrackAnalysis.R
+++ HermioneLastfmHistoryAndSpotifyTrackAnalysis.R
@@ -2,6 +2,7 @@
 #To do this, you need to get the last.fm data first. to do that we can merge the data from another R script
 #We need to update this so that it works for the entire list of 1700 songs and artists. Also need to be able to resolve all the instances of garbled data-- maybe my code already does this?
 #Find track spotify id, audio features, from song artist and title
+packrat::init()
 install.packages("tidyverse")
 install.packages("devtools")
 devtools::install_github('charlie86/spotifyr')
@@ -10,8 +11,10 @@ install.packages("purrr")
 install.packages("dplyr")
 install.packages("Rcpp")
 install.packages("knitr")
+install.packages("packrat")
 
 #Added Library installation of package "Rcpp" due to error code in work computer-- may not need if we install Rtools first
+library(packrat)
 library(tidyverse)
 library(devtools)
 library(spotifyr)
@@ -21,6 +24,7 @@ library(dplyr)
 library(Rcpp)
 library(knitr)
 library(lubridate)
+packrat::snapshot()
 
 #This is how we create function for searching spotify to get song features
 track_audio_features <- function(artist, title, type = "track") {
