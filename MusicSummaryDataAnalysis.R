803227517ce04c15da277fada2a82d2b6948b06c
Drew <Drew@MacBook-Pro.local>
2020-03-09 23:17:28 -0400

    Added summary r script to start manipulating data, goal to make new dataframe with daily averages

---
+++ MusicSummaryDataAnalysis.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(lubridate)
+
+
+
+
+my_data -> Drewdata
+
+Drewdata <- read.csv("DrewMusic 09-Mar-2020 22.26 .csv")
+as.data.frame(Drewsummary)
+
+view(Drewsummary)
+
+summary <- Drewdata %>% 
+  summarise()
+
+view(as.table(Drewsummary))
+
+out <- capture.output(summary.data.frame(Drewdata))
+cat("Drewsummary", out, file="Drewsummaries.txt", sep="n", append=TRUE)
+
+#Taking summary making it an r object, table chr [1:7, 1:27]
+Drewsummary <- summary.data.frame(Drewdata)
+
+Drewsummary %>% 
+  select(Var2,Freq)
+
+
+newdf <- select(Drewsummary, 
+
+
+#Make summary file
+write.csv(as.table(Drewsummary),"Drewsummary")
+
+write.table(Drewsummary, file = "summary.txt")
+
+#
+
+sink(Drewsummary)
+print(Drewsummary)
+sink()  #
+
+
+
+Drewdata
+
+view(summary)
+
+as.Date.character(Drewdata$date)
+
+Drewdata %>% 
+  mutate(date <-)
+  group_by(date) %>% 
+  
+  summarise(…)
+
+  =
+dateplayed <- as.Date(Drewdata$date)
