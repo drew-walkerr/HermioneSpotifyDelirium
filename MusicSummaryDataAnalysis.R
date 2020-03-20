153cd2df1ba9ac5dd028335b11137dba15eacb91
Drew <thedrewwalker@gmail.com>
2020-03-19 20:25:05 -0400

    added the last theoretical ends variables to set which helps identifying the listening sessions

--- MusicSummaryDataAnalysis.R
+++ MusicSummaryDataAnalysis.R
@@ -12,10 +12,78 @@ Drewdata <- read.csv("DrewMusic 09-Mar-2020 22.26 .csv")
 as_datetime(Drewdata$date)
 # Separating Dates into Components, adding Year, month, day variables ----------------------------------------
 
-Drewdata <- Drewdata %>% 
-  mutate(year = year(date), month = month(date), day = day(date), hour = hour(date))
+#Convert duration_ms to millisecond period value 
+milli <- dmilliseconds(Drewdata$duration_ms)
+#WOrkable sparsing hours and days and months 
 
-Drewdata
+# WORKING ANALYSIS TRACK SESSION CODE -------------------------------------
+
+
+Drewdata <- Drewdata %>% 
+  mutate(year = year(date), 
+         month = month(date), 
+         day = day(date), 
+         hour = hour(date),
+         last_play = lead(date), 
+         recentstart = as_datetime(date),
+         end = (as_datetime(date) + milli),
+         last_theo_end = lead(end))
+         
+time.interval <- Drewdata$last_theo_end %--% Drewdata$recentstart
+
+
+Drewdata <- Drewdata %>% 
+  mutate(diff = as.duration(time.interval),
+         
+  
+         diff = dmilliseconds(Drewdata$last_play - Drewdata$recentstart)
+  )
+
+str(time.interval)
+
+as.duration(time.interval)
+
+summary(Drewdata$diff)
+
+#Workable interval code
+
+Drewdata <- Drewdata %>% 
+  mutate(last_play = lead(date), start = date, end = (as_datetime(date) + milli))
+
+#Trying differences
+
+typeof(Drewdata$start)
+
+Drewdata <- Drewdata %>% 
+  mutate(diff = dmilliseconds(Drewdata$start %--% Drewdata$lagged_date))
+
+typeof(Drewdata$start)
+typeof(lagged_date)
+a
+#funky
+diff = start - lagged_date,
+new_interval = diff > 5,
+new_interval = ifelse(is.na(new_interval), FALSE, new_interval),
+interval_number = cumsum(new_interval))
+
+
+typeof(Drewdata$duration_ms)
+
+asinte
+
+
+
+Drewdata <- Drewdata %>% 
+  mutate(end = (as_datetime(date) + milli))
+
+, start = date, end = (date + duration_ms))d
+
+as_datetime(Drewdata$date) + milli
+
+
+# VISUALIZATIONS ----------------------------------------------------------
+
+
 
 ggplot(data = Drewdata) +
   geom_point(mapping = aes(x = hour, y = valence, color = year))
@@ -27,7 +95,9 @@ Drewdata %>%
 
 summary(Drewdata$hour)
 
-hist(Drewdata$hour)
+hist(Drewdata$year, )
+
+plot(Drewdata$year)
 
 # DREWDATA VISUALIZATIONS -----------------------------------------------------
 
@@ -91,8 +161,33 @@ Drewdata %>%
   summarise(…)
 
   =
-dateplayed <- as.Date(Drewdata$date)
+
+
+# DrewData Time Listening sessions ----------------------------------------
+dateplayed <- as.Datetime(Drewdata$date)
   
+as_datetime(Drewdata$date)
+  
+i <- interval()
+
+typeof(Drewdata$duration_ms)
+
+
+Drewdata <- 
+  Drewdata %>% 
+  mutate(lagged_date = lag(date))
+
+, start = date, end = (date + duration_ms))d
+
+#new 
+Df %>% 
+        diff = start - lagged_end,
+         new_interval = diff > 5,
+         new_interval = ifelse(is.na(new_interval), FALSE, new_interval),
+         interval_number = cumsum(new_interval))
+  
+  
+Drewdata
 
 # Shelton Data ------------------------------------------------------------
 SheltonData <- read.csv("SheltonMusic 14-Mar-2020 18.47 .csv")
